cc62671c30847af6e2540d369d72832dd40b752e
joao carlos <163042743+d4sjoao@users.noreply.github.com>
2024-09-13 09:39:18 -0300

    Novas alterações de testes

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos') //Lembrando: O visit está encurtado, por conta de ter colocado o link lá em "cypress.config.js"
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.products > .row')
+        //.First()
+        //.last()
+        //.eq(5) // Eq serve para listar algum produto que eu quiser, apontando com o número dele na tela, para ser clicável.
+        .contains('Ariel Roll Sleeve Sweatshirt') //Aqui, vai selecionar o que contém da página.
+        .click()
+        cy.get('#tab-title-description > a').should('exist')
+    });
+});
